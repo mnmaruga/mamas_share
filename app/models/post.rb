0790e9d25c174e38bc8c9c3e112ca0f118454fc2
mnmaruga <toi_moi.em@icloud.com>
2020-10-02 16:55:49 +0900

    アソシエーションとバリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
-  
+  belongs_to :user
+  has_many :comments
+
+  with_options presence :true do
+    validates :user
+    validates :room
+    validates :problem ,length: { max: 20 } 
+    validates :detail
+  end
 end
