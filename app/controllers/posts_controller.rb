c25333417211b15fba35456ae4f07b062b49fc45
mnmaruga <toi_moi.em@icloud.com>
2020-10-06 00:44:43 +0900

    ストロングパラメータの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:room, :problem, :detail).merge(user_id: current_user.id)
+  end
+
 end
