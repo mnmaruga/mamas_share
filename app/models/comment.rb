4814a905b06cbba280d2e985158a034f1675a247
mnmaruga <toi_moi.em@icloud.com>
2020-10-06 18:19:43 +0900

    アソシエーション、バリデーションの設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to: user
+  belongs_to: posts
+
+  with_options presence: true do
+    validates :user
+    validates :room
+    validates :comment, length: { maximum: 400 }
+  end
 end
