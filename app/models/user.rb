51b85148c70a41b2161de1498411a1599895d23e
mnmaruga <toi_moi.em@icloud.com>
2020-10-06 18:51:32 +0900

    アソシエーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many: posts
+  has_many: comments
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :status
 
